0a2caf35fc0bb09e6406e27c181de75903d3ba5a
Richard Kendall <richardjkendall@users.noreply.github.com>
2021-10-17 14:11:43 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,12 +13,30 @@ if [ "$check_output" == "$okay_string" ]; then
   mkdir -p ~/.local/bin/kubectl
   mv ./kubectl ~/.local/bin/kubectl
   chmod +x ~/.local/bin/kubectl/kubectl
-  exit 0
 else
   echo "ERROR: checksum does not match"
   exit 1
 fi
 
 echo "DOWNLOAD: img"
+
 curl -fSL "http://webshare-svc.default.svc.cluster.local/img" -o "/usr/local/bin/img"
+if [ $? -eq 1 ]
+  echo "ERROR: could not download img binary"
+  exit 1
+then
+  echo "OKAY: downloaded img"
+fi
+
+check_output=`echo "3df3d9de798919c34bb3285518cf6f19307c9c7a0d697d3ede6a79dc51af2b55 /usr/local/bin/img" | sha256sum --check`
+okay_string="img: OK"
+
+if [ "$check_output" == "$okay_string" ]; then
+  echo "OKAY: downloaded file matches the expected checksum"
+else
+  echo "ERROR: checksum does not match"
+  exit 1
+fi
+
 chmod a+x /usr/local/bin/img
+echo "OKAY: img is downloaded and installed."
