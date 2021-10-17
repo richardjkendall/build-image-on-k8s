08d0dfa29b5997cf84a94cd86f79cd8c9010b331
Richard Kendall <richardjkendall@users.noreply.github.com>
2021-10-17 14:29:31 +1100

    Fix syntax

--- install.sh
+++ install.sh
@@ -22,14 +22,14 @@ echo "OKAY: kubectl is downloaded and installed."
 
 echo "DOWNLOAD: img"
 curl -LO "http://webshare-svc.default.svc.cluster.local/img"
-if [ $? -eq 1 ]l; then
+if [ $? -eq 1 ]; then
   echo "ERROR: could not download img binary"
   exit 1
 else
   echo "OKAY: downloaded img"
 fi
 
-check_output=`echo "3df3d9de798919c34bb3285518cf6f19307c9c7a0d697d3ede6a79dc51af2b55 /usr/local/bin/img" | sha256sum --check`
+check_output=`echo "3df3d9de798919c34bb3285518cf6f19307c9c7a0d697d3ede6a79dc51af2b55 img" | sha256sum --check`
 okay_string="img: OK"
 
 if [ "$check_output" == "$okay_string" ]; then
